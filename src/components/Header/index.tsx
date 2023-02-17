dfc01360cd742d2a9bc9cdb0164b1824e1d3e4d4
leticiar-io <leticia.rodrigues20019@gmail.com>
2023-02-17 17:34:24 -0300

    creating an accessible modal

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,30 @@
 import { HeaderContainer, HeaderContent, NewTransactionButton } from "./styles";
-import logoImg from '../../assets/logo-name.svg'
+import logoImg from "../../assets/logo-name.svg";
+import * as Dialog from "@radix-ui/react-dialog";
 
 export function Header() {
-   return(
-      <HeaderContainer>
-         <HeaderContent>
-            <img src={logoImg} alt="" />
+  return (
+    <HeaderContainer>
+      <HeaderContent>
+        <img src={logoImg} alt="" />
 
+        <Dialog.Root>
+          {/*It's a context */}
+          <Dialog.Trigger asChild>
             <NewTransactionButton>Nova transação</NewTransactionButton>
-         </HeaderContent>
-      </HeaderContainer>
-   )
+          </Dialog.Trigger>
+
+          <Dialog.Portal>
+            <Dialog.Overlay />
+
+            <Dialog.Content>
+               <Dialog.Title>Nova transação</Dialog.Title>
+
+               <Dialog.Close />
+            </Dialog.Content>
+          </Dialog.Portal>
+        </Dialog.Root>
+      </HeaderContent>
+    </HeaderContainer>
+  );
 }
