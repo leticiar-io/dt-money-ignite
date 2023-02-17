d15375f6203910d5db97c39c382e3222968f631a
leticiar-io <leticia.rodrigues20019@gmail.com>
2023-02-17 17:00:21 -0300

    transaction table

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,11 +1,47 @@
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
+import {
+  TransactionsContainer,
+  TransactionsTable,
+  PriceHighlight,
+} from "./styles";
 
 export function Transactions() {
-   return(
-      <div>
-         <Header />
-         <Summary />
-      </div>
-   )
+  return (
+    <div>
+      <Header />
+      <Summary />
+
+      <TransactionsContainer>
+        <TransactionsTable>
+          <tbody>
+            <tr>
+              <td width="50%">Desenvolvimento de site</td>
+              <td>
+                <PriceHighlight variant={"income"}>R$ 12.000,00</PriceHighlight>
+              </td>
+              <td>Venda</td>
+              <td>13/06/2022</td>
+            </tr>
+            <tr>
+              <td width="50%">Shein</td>
+              <td>
+                <PriceHighlight variant={"outcome"}>- R$ 300,00</PriceHighlight>
+              </td>
+              <td>Compra</td>
+              <td>10/06/2022</td>
+            </tr>
+            <tr>
+              <td width="50%">Pizza</td>
+              <td>
+                <PriceHighlight variant={"outcome"}>- R$ 80,00</PriceHighlight>
+              </td>
+              <td>Alimentação</td>
+              <td>12/06/2022</td>
+            </tr>
+          </tbody>
+        </TransactionsTable>
+      </TransactionsContainer>
+    </div>
+  );
 }
