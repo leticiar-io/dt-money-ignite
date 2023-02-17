083550835e440f1c41da0a3aa45a91c539074448
leticiar-io <leticia.rodrigues20019@gmail.com>
2023-02-17 18:39:37 -0300

    new transition modal

---
+++ src/components/NewTransactionModal/index.tsx
@@ -0,0 +1,25 @@
+import * as Dialog from "@radix-ui/react-dialog";
+import { X } from "phosphor-react";
+import { Overlay, Content, CloseButton } from "./styles";
+
+export function NewTransactionModal() {
+  return (
+    <Dialog.Portal>
+      <Overlay />
+
+      <Content>
+        <Dialog.Title>Nova transação</Dialog.Title>
+        <CloseButton>
+          <X size={24}/>
+        </CloseButton>
+        <form action="">
+          <input type="text" placeholder="Descrição" required />
+          <input type="number" placeholder="Preço" required />
+          <input type="text" placeholder="Categoria" required />
+
+          <button type="submit">Cadastrar</button>
+        </form>
+      </Content>
+    </Dialog.Portal>
+  );
+}
