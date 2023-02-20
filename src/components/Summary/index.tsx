0ec242e9604d3934dffe253b3887b764826fb5e0
leticiar-io <leticia.rodrigues20019@gmail.com>
2023-02-20 17:02:17 -0300

    formatting values

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,26 +1,30 @@
 import { ArrowCircleUp, ArrowCircleDown, CurrencyDollar } from "phosphor-react";
 import { useContext } from "react";
 import { TransactionsContext } from "../../context/TransactionsContext";
+import { priceFormatter } from "../../utils/formatter";
 import { SummaryContainer, SummaryCard } from "./styles";
 
 export function Summary() {
   const { transactions } = useContext(TransactionsContext);
 
   const summary = transactions.reduce(
-   (acc, transaction) => {
-      if(transaction.type === 'income') {
-         acc.income += transaction.price;
-         acc.total += transaction.price;
+    (acc, transaction) => {
+      if (transaction.type === "income") {
+        acc.income += transaction.price;
+        acc.total += transaction.price;
       } else {
-         acc.outcome += transaction.price;
-         acc.total -= transaction.price;
+        acc.outcome += transaction.price;
+        acc.total -= transaction.price;
       }
       return acc;
-  }, {
-    income: 0,
-    outcome: 0,
-    total: 0,
-  });
+    },
+    {
+      income: 0,
+      outcome: 0,
+      total: 0,
+    }
+  );
+
   return (
     <SummaryContainer>
       <SummaryCard>
@@ -29,7 +33,7 @@ export function Summary() {
           <ArrowCircleUp size={32} color="#00b37e" />
         </header>
 
-        <strong>{summary.income}</strong>
+        <strong>{priceFormatter.format(summary.income)}</strong>
       </SummaryCard>
       <SummaryCard>
         <header>
@@ -37,7 +41,7 @@ export function Summary() {
           <ArrowCircleDown size={32} color="#f75a68" />
         </header>
 
-        <strong>{summary.outcome}</strong>
+        <strong>- {priceFormatter.format(summary.outcome)}</strong>
       </SummaryCard>
       <SummaryCard variant="green">
         <header>
@@ -45,7 +49,7 @@ export function Summary() {
           <CurrencyDollar size={32} color="#fff" />
         </header>
 
-        <strong>{summary.total}</strong>
+        <strong>{priceFormatter.format(summary.total)}</strong>
       </SummaryCard>
     </SummaryContainer>
   );
